514c27dc5b96604c2ae340500cbb865aef0724bd
RehanAnsari17 <165806706+RehanAnsari17@users.noreply.github.com>
2025-08-03 15:10:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-CLUSTER_URL       = 'https://c2b883ba-be1e-482f-813d-bf7d79b3cb48.europe-west3-0.gcp.cloud.qdrant.io:6333'
-QDRANT_API_KEY    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.b9q7l3ldLc-HQiS5MJoS93AVO72Mye-WJSFvtGHM7V8'
-COLLECTION_NAME   = 'my_collection'
+CLUSTER_URL       = 'add yours'
+QDRANT_API_KEY    = 'add yours'
+COLLECTION_NAME   = 'add yours'
 
 TEMPERATURE       = 0.5
 MODEL_NAME        = 'llama3-8b-8192'
-GROQ_API_KEY      = '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'
+GROQ_API_KEY      = 'add yours'
